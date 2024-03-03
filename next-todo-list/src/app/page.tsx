b8b4e221f6755737a9c3b439613c283e33d1759d
Russell258 <duckie258@gmail.com>
2024-03-04 02:41:34 +0800

    added components and async function

--- next-todo-list/src/app/page.tsx
+++ next-todo-list/src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home(){
-  return <h1>Hi</h1>
+import { TodoItem } from "@/components/TodoItem";
+import { prisma } from "@/db";
+import Link from "next/link";
+
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+export default async function Home(){
+
+  const todos = await getTodos();
+
+  return (<>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+    </header>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <TodoItem key={todo.id} {...todo} />
+      ))}
+    </ul>
+
+  </>
+  )
 }
