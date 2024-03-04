24c8af8a6ec1acfdfdb54c0dbca8c5cc7ffdeef1
Russell258 <duckie258@gmail.com>
2024-03-05 01:16:12 +0800

    defaultchecked, async, prisma updat

--- next-todo-list/src/app/new/page.tsx
+++ next-todo-list/src/app/new/page.tsx
@@ -1,3 +1,42 @@
+import { prisma } from "@/db";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+async function createTodo(data: FormData){
+    "use server"
+
+    //use server^^ states that it is going to be server actions
+
+    // get the value of the form and pass it into the db
+    const title = data.get("title")?.valueOf()
+    if (typeof title!=="string" || title.length===0){
+        throw new Error("Invalid Title");
+    }
+
+    //create the data with prisma
+    await prisma.todo.create({data: {title, complete: false}});
+    redirect("/");
+}
+
 export default function Page(){
-    return <h1>New</h1>
+    return <>
+    <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+    </header>
+    <form action={createTodo} className="flex gap-2 flex-col">
+        <input 
+            type="text" 
+            name="title"
+            className="border border-slate-300 bg-transparent rounded px-2 py-1
+            outline-none focus-within:border-slate-100" />
+    <div className="flex gap-1 justify-end">
+        <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+            Cancel
+        </Link>
+        <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+            Create
+        </button>
+    </div>
+    </form>
+    </>
 }
--- next-todo-list/src/app/page.tsx
+++ next-todo-list/src/app/page.tsx
@@ -6,6 +6,13 @@ function getTodos(){
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id:string, complete: boolean){
+  "use server"
+
+  //sql query update todo through prisma, where the id matches, update the complete.
+  await prisma.todo.update({where: {id}, data: {complete}});
+}
+
 export default async function Home(){
 
   const todos = await getTodos();
@@ -17,7 +24,7 @@ export default async function Home(){
     </header>
     <ul className="pl-4">
       {todos.map(todo => (
-        <TodoItem key={todo.id} {...todo} />
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
       ))}
     </ul>
 
